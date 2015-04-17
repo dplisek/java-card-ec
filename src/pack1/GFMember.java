7288087568ee539cd363ecadb805a348495ae4e8
Dominik <Dominik@Win8vrt>
2015-04-17 18:28:57 +0200

    squaring works

--- src/pack1/GFMember.java
+++ src/pack1/GFMember.java
@@ -6,6 +6,7 @@
 package pack1;
 
 import javacard.framework.JCSystem;
+import javacard.framework.Util;
 
 /**
  *
@@ -14,6 +15,8 @@ import javacard.framework.JCSystem;
 public class GFMember {
 
     private final byte[] bytes = JCSystem.makeTransientByteArray(Applet1.FIELD_WIDTH_BYTES, JCSystem.CLEAR_ON_RESET);
+    private final byte[] squaringHelperBytes = JCSystem.makeTransientByteArray((short) (2 * Applet1.FIELD_WIDTH_BYTES), JCSystem.CLEAR_ON_RESET);
+    private GFSquaringTable squaringTable = new GFSquaringTable();
 
     public byte[] getBytes() {
         return bytes;
@@ -42,16 +45,37 @@ public class GFMember {
             bytes[i] = (byte) 0x00;
         }
     }
-    
+
     public void setOne() {
         for (short i = 0; i < Applet1.FIELD_WIDTH_BYTES - 1; i++) {
             bytes[i] = (byte) 0x00;
         }
         bytes[Applet1.FIELD_WIDTH_BYTES - 1] = (byte) 0x01;
     }
-    
+
     public void squared(GFMember out) {
-        // TODO
+        for (short i = 0; i < Applet1.FIELD_WIDTH_BYTES; i++) {
+            byte toSplit = bytes[i];
+            byte upper = (byte) ((toSplit >>> 4) & 15);
+            byte lower = (byte) (toSplit & 15);
+            squaringHelperBytes[(short) (2 * i)] = squaringTable.getSquared(upper);
+            squaringHelperBytes[(short) (2 * i + 1)] = squaringTable.getSquared(lower);
+        }
+        
+        for (short i = 0; i < squaringHelperBytes.length - Applet1.FIELD_WIDTH_BYTES; i++) {
+            byte highestXorPattern = (byte) ((squaringHelperBytes[i] >>> 6) & 3);
+            byte middleXorPattern = (byte) ((squaringHelperBytes[i] << 2));
+            byte lowestXorPattern = (byte) (squaringHelperBytes[i] << 1);
+            squaringHelperBytes[(short) (i + 8)] ^= highestXorPattern;
+            squaringHelperBytes[(short) (i + 9)] ^= middleXorPattern;
+            squaringHelperBytes[(short) (i + 10)] ^= lowestXorPattern;
+        }
+        
+        // WARNING: Ignoring final xor of overlapping byte here, because only the first bit of it is to be reduced,
+        // and the first bit is always zero, as the bytes are interleaved with zeros. This is not generally correct, but works here
+        // and makes the algorithm shorter.
+        
+        Util.arrayCopyNonAtomic(squaringHelperBytes, Applet1.FIELD_WIDTH_BYTES, out.getBytes(), (short) 0, Applet1.FIELD_WIDTH_BYTES);
     }
 
     public void times(GFMember other, GFMember out) {
---
+++ src/pack1/GFSquaringTable.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pack1;
+
+import javacard.framework.JCSystem;
+
+/**
+ *
+ * @author Dominik
+ */
+public class GFSquaringTable {
+    
+    private byte[] squaredHalfBytes = JCSystem.makeTransientByteArray((short) 16, JCSystem.CLEAR_ON_RESET);
+    
+    public GFSquaringTable() {
+        squaredHalfBytes[0] = (byte) 0;
+        squaredHalfBytes[1] = (byte) 1;
+        squaredHalfBytes[2] = (byte) 4;
+        squaredHalfBytes[3] = (byte) 5;
+        squaredHalfBytes[4] = (byte) 16;
+        squaredHalfBytes[5] = (byte) 17;
+        squaredHalfBytes[6] = (byte) 20;
+        squaredHalfBytes[7] = (byte) 21;
+        squaredHalfBytes[8] = (byte) 64;
+        squaredHalfBytes[9] = (byte) 65;
+        squaredHalfBytes[10] = (byte) 68;
+        squaredHalfBytes[11] = (byte) 69;
+        squaredHalfBytes[12] = (byte) 80;
+        squaredHalfBytes[13] = (byte) 81;
+        squaredHalfBytes[14] = (byte) 84;
+        squaredHalfBytes[15] = (byte) 85;
+    }
+    
+    public byte getSquared(byte halfByte) {
+        return squaredHalfBytes[halfByte];
+    }
+}
